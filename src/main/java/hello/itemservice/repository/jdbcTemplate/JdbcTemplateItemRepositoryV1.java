453c59fa93bdea713ddc5a5bc5deb6768314950c
wjcho <wj930303@gmail.com>
2024-07-27 16:05:44 +0900

    Feat: JdbcTemplate 적용을 위한 Repository 파일 생성 및 생성자 주입

---
+++ src/main/java/hello/itemservice/repository/jdbcTemplate/JdbcTemplateItemRepositoryV1.java
@@ -0,0 +1,43 @@
+package hello.itemservice.repository.jdbcTemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * JdbcTemplate
+ */
+public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateItemRepositoryV1(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Item save(Item item) {
+        return null;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        return null;
+    }
+}
