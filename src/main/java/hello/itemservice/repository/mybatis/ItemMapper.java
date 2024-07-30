d798cb604b6ea4538fa40bb458c0f63d871443eb
wjcho <wj930303@gmail.com>
2024-07-30 23:43:05 +0900

    Fix: 오타 수정

--- src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
+++ src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -14,7 +14,7 @@ public interface ItemMapper {
 
     void save(Item item);
 
-    void update(@Param("id") Long id, @Param("updateParam")ItemUpdateDto updateParam);
+    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
 
     List<Item> findAll(ItemSearchCond itemSearch);
 
