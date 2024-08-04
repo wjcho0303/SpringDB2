ed2de66a0ec45f906afb5bf1da4c44b913756151
wjcho <wj930303@gmail.com>
2024-08-04 19:19:00 +0900

    Fix: Spring Data JPA Repository를 의존하는 ItemRepository 구현체 클래스 V2 생성

---
+++ src/main/java/hello/itemservice/repository/springDataJPA/JpaItemRepositoryV2.java
@@ -0,0 +1,56 @@
+package hello.itemservice.repository.springDataJPA;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class JpaItemRepositoryV2 implements ItemRepository {
+
+    private final SpringDataJpaItemRepository repository;
+
+    @Override
+    public Item save(Item item) {
+        return repository.save(item);
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item foundItem = repository.findById(itemId).orElseThrow();
+        foundItem.setItemName(updateParam.getItemName());
+        foundItem.setPrice(updateParam.getPrice());
+        foundItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        if (StringUtils.hasText(itemName) && maxPrice != null) {
+//            return repository.findByItemNameLikeAndPriceLessThanEquals(itemName, maxPrice);
+            return repository.findItems(itemName, maxPrice);
+        } else if (StringUtils.hasText(itemName)) {
+            return repository.findByItemNameLike(itemName);
+        } else if (maxPrice != null) {
+            return repository.findByPriceLessThanEquals(maxPrice);
+        } else {
+            return repository.findAll();
+        }
+    }
+}
--- src/main/java/hello/itemservice/repository/springDataJPA/SpringDataJpaItemRepository.java
+++ src/main/java/hello/itemservice/repository/springDataJPA/SpringDataJpaItemRepository.java
@@ -2,6 +2,22 @@ package hello.itemservice.repository.springDataJPA;
 
 import hello.itemservice.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNameLike(String itemName);
+
+    List<Item> findByPriceLessThanEquals(Integer price);
+
+    // 쿼리 메서드 자동생성 방식
+    List<Item> findByItemNameLikeAndPriceLessThanEquals(String itemName, Integer price);
+
+    // @Query 어노테이션 방식
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+
 }
