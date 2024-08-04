1b3593b015326cfb7dacb9ad86eea82df983bcbe
wjcho <wj930303@gmail.com>
2024-08-04 22:34:26 +0900

    Feat: QuerydslConfig 적용 및 동적 쿼리 리팩토링

--- src/main/java/hello/itemservice/config/QuerydslConfig.java
+++ src/main/java/hello/itemservice/config/QuerydslConfig.java
@@ -1,23 +1,24 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.springDataJPA.JpaItemRepositoryV2;
-import hello.itemservice.repository.springDataJPA.SpringDataJpaItemRepository;
+import hello.itemservice.repository.querydsl.JpaItemRepositoryV3;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+
 @Configuration
 @RequiredArgsConstructor
-public class SpringDataJpaConfig {
+public class QuerydslConfig {
 
-    private final SpringDataJpaItemRepository repository;
+    private final EntityManager em;
 
     @Bean
     public ItemRepository itemRepository() {
-        return new JpaItemRepositoryV2(repository);
+        return new JpaItemRepositoryV3(em);
     }
 
     @Bean
